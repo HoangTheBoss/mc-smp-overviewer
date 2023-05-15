947d7c835c309f5f9c48b4ba7c24d18d14c4b255
Nguyen Hoang <hoangtheboss.hyper@gmail.com>
2023-05-15 13:46:33 +0000

    change texture

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1684141567",
+        "cacheTag": "1684151397",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
