1030a7871c33c34b503c70547116242393da1b4a
Nguyen Hoang <hoangtheboss.hyper@gmail.com>
2023-05-21 08:53:21 +0700

    update map

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1684631919",
+        "cacheTag": "1684634000",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
